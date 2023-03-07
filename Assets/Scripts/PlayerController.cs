b2894cf4c4ab3c32d818b35ae06677001ff90ba8
Natalia DaLomba <nataliadalomba@protonmail.com>
2023-03-06 23:10:59 -0500

    Adding player Jump animation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
+using UnityEngine.Animations;
 
 public class PlayerController : MonoBehaviour {
-
 
     private float horizontal;
     private float speed = 8f;
@@ -18,14 +18,9 @@ public class PlayerController : MonoBehaviour {
 
         animator.SetFloat("Speed", Mathf.Abs(horizontal));
 
-        if (Input.GetButtonDown("Jump") && IsGrounded()) {
-            rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
-        }
 
-        //this allows us to jump higher by pressing the jump button longer, and jump lower by just tapping the jump button
-        if(Input.GetButtonDown("Jump") && rb.velocity.y > 0f)
-            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
-
+        IsJumping();
+        IsRunning();
         Flip();
     }
 
@@ -37,12 +32,34 @@ public class PlayerController : MonoBehaviour {
         return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
     }
 
+    private void IsRunning() {
+
+        Debug.Log("Gravity scale trying to set to 10");
+        if((Input.GetKeyDown("left") || Input.GetKeyDown("right")) && IsGrounded()) {
+            rb.gravityScale = 10f;
+        }
+    }
+
+    private void IsJumping() {
+        if (Input.GetButtonDown("Jump") && IsGrounded()) {
+            rb.gravityScale = 4f;
+            rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+        }
+
+        //this allows us to jump higher by pressing the jump button longer, and jump lower by just tapping the jump button
+        if(Input.GetButtonDown("Jump") && rb.velocity.y > 0f) {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+            animator.SetBool("Jumping", true);
+        }
+    }
+
     private void Flip() {
         if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f) {
             isFacingRight = !isFacingRight;
             Vector3 localScale = transform.localScale;
             localScale.x *= -1f;
             transform.localScale = localScale;
+            //transform.parent.localScale = localScale;
         }
     }
 }
